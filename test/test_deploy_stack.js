e408ac4ac943aaa7a691f368f625caccb171ec20
gkpty <gkardonski@gmail.com>
2020-03-29 21:57:52 -0500

    started async but hosted zone is executing after return... need to see async

--- test/test_deploy_stack.js
+++ test/test_deploy_stack.js
@@ -1,6 +1,9 @@
 var generateTemplate = require('../lib/generateTemplate')
 var validateTemplate = require('./validateTemplate')
 
-generateTemplate('supereasyforms.com', 'index.html','error.html', true, true, true).then((data) => {
-  if(data) validateTemplate(data.template)
-})
+async function test(){
+  await generateTemplate('supereasyforms.com', 'index.html','error.html', true, true, true)
+  await console.log(console.log('hIPYYSJS'))
+}
+
+test()
