d928ec91ed58dcef959911e292313db877b50f5c
gkpty <gkardonski@gmail.com>
2020-03-30 18:31:03 -0500

    added changes

--- test/test_deploy_stack.js
+++ test/test_deploy_stack.js
@@ -1,3 +1,3 @@
 var deploySite = require('../lib/deploySite')
 
-deploySite('toruscms.com', 'index.html','error.html', true, true, true)
+deploySite('gkpty.com', 'index.html','error.html', true, true, true)
