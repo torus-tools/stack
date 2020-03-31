9d79e2a40e5dc06f52b3b7375d75694d319fef5f
Gabriel Kardonski <31138615+gkpty@users.noreply.github.com>
2020-03-30 22:16:51 -0500

    Revert "Basic cf site"

--- index.js
+++
@@ -1,6 +0,0 @@
-var generateTemplate = require('./lib/generateCloudformationTemplate')
-var deployTemplate = require('././lib/deployBasicTemplate')
-
-let template = generateTemplate('gkpty.com', 'index.html', false)
-
-deployTemplate('gkpty.com', JSON.stringify(template))
