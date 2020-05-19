53e760b27b5e2713b11197f4fa5eae8789c7ad74
gkpty <gkardonski@gmail.com>
2020-05-18 20:06:52 -0500

    restructured some stuff and wrote the index

--- index.js
+++ index.js
@@ -1,7 +1,30 @@
-var generateTemplate = require('./lib/generateCloudformationTemplate')
-var deployTemplate = require('././lib/deployBasicTemplate')
+const generateTemplate = require('./lib/generateTemplate');
+const deploySite = require('././lib/deploySite');
+const deleteSite = require('./lib/deleteSite');
+const stack = require('./lib/deployStack');
+const certificate = require('./lib/acmCertificate');
+const exists = require('./lib/checkResource');
+const upload = require('./lib/upload');
 
-module.exports = {
-  deployTemplate,
-  generateTemplate
-}
+module.exports.generateTemplate = generateTemplate;
+module.exports.deployStack = stack.deployStack;
+module.exports.createChangeSet = stack.createChangeSet;
+
+module.exports.requestCertificate = certificate.requestCertificate;
+module.exports.describeCertificate = certificate.describeCertificate;
+module.exports.validateCertificate = certificate.validateCertificate;
+module.exports.createCertificate = certificate.createCertificate;
+module.exports.importCertificate = certificate.importCertificate;
+
+module.exports.stackExists = exists.stackExists;
+module.exports.bucketExists = exists.bucketExists;
+module.exports.distributionExists = exists.distributionExists;
+module.exports.hostedZoneExists = exists.hostedZoneExists;
+
+module.exports.deleteSite = deleteSite;
+module.exports.deploySite = deploySite;
+
+module.exports.uploadFile = upload.uploadFile;
+module.exports.uploadSite = upload.uploadDir;
+
+
