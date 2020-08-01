d31f5dbda0fb859c955c65e598a2c24b8a6db029
gkpty <gkardonski@gmail.com>
2020-07-31 22:12:42 -0500

    refactored the generateTemplate method and also the checkRouserce methods. Test for generateTemplate is passing. also refactored some of the DNS methods to the domains repo and some other methods

--- test/generateTemplateTest.js
+++ test/generateTemplateTest.js
@@ -5,8 +5,6 @@ var generateTemplate = require('../lib/generateTemplate')
 var AWS = require('aws-sdk');
 var cloudformation = new AWS.CloudFormation({apiVersion: '2010-05-15'});
 
-let templi = {"AWSTemplateFormatVersion":"2010-09-09","Resources":{"BucketPolicy":{"Type":"AWS::S3::BucketPolicy","Properties":{"Bucket":"testingsite.com","PolicyDocument":{"Version":"2012-10-17","Statement":[{"Sid":"PublicReadGetObject","Effect":"Allow","Principal":"*","Action":"s3:GetObject","Resource":"arn:aws:s3:::testingsite.com/*"}]}}},"RootBucket":{"Type":"AWS::S3::Bucket","Properties":{"AccessControl":"PublicRead","BucketName":"testingsite.com","WebsiteConfiguration":{"ErrorDocument":"error.html","IndexDocument":"index.html"}}}}}
-
 let stack = {
   bucket: true,
   www: true,
@@ -21,10 +19,10 @@ let options = {
 }
 
 describe('Check the generateTemplate method', function() {
-  let domain = "test.com";
+  let domain = "www.test.com";
   describe('No params supplied',()=>{
     it('Should produce an error', ()=>{
-     generateTemplate().catch(err => assert.equal(err.includes('invalid domain'), true))
+     generateTemplate().catch(err => assert.equal(err.includes('Error: Please use a valid domain name'), true))
     })
   });
   describe('Validates a generated cloudFromation template for a prod setup', ()=>{
