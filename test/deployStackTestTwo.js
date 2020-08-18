64a4f217b3544cb5f5f8bc9e074d6fb3296ccd4a
gkpty <gkardonski@gmail.com>
2020-08-18 04:10:19 -0500

    started fixing issue in template generatetion. it was overwriting the partialtemplate with a full template

---
+++ test/deployStackTestTwo.js
@@ -0,0 +1,24 @@
+const {deployStack} = require('../lib/deployStack')
+
+const stack = {
+  bucket: true,
+  www: true,
+  dns: true,
+  cdn: true,
+  https: false
+}
+
+const config = {
+  index:"index.html",
+  error:"error.html",
+  last_deployment:"",
+  providers: {
+    domain: 'godaddy',
+    bucket: 'aws',
+    cdn: 'aws',
+    dns: 'godaddy',
+    https: 'aws'
+  }
+}
+
+deployStack('localizehtml.com', stack, config, true)
